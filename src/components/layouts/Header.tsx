d857e065b5818efe89501082948d8a117227196d
florent.nkita <flo95400@gmail.com>
2026-07-02 16:58:03 +0200

    style: Hide MapPin Icon

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -25,10 +25,10 @@ export default function Header() {
       <div className="w-full py-4 px-6 flex items-center justify-between">
         {/* Partie gauche */}
         <div className="hidden lg:flex items-center gap-4 flex-1">
-          <MapPin className="w-5 h-5" />
+          {/* <MapPin className="w-5 h-5" /> */}
           <Link
             to="/recrutement"
-            className="bg-red-600 hover:bg-red-700 transition-colors text-white px-4 py-2 rounded-full font-bold text-sm"
+            className="bg-red-600 hover:bg-red-700 transition-colors text-white ml-8 px-4 py-2 rounded-full font-bold text-sm"
           >
             Nous Recrutons
           </Link>
